0b8ae88e0711742fda62a307fa51677a43ce41b0
mac <841146404@qq.com>
2023-01-29 14:30:09 +0800

    v1

--- src/JPush.php
+++ src/JPush.php
@@ -2,11 +2,18 @@
 
 namespace shenyonghang\message;
 
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
 class JPush
 {
 
-   public function test(){
-       echo "test1";
+   public function send(){
+       $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest','message');
+       $channel = $connection->channel();
+       $channel->queue_declare('hello', false, true, false, false);
+       $msg = new AMQPMessage(123);
+       $channel->basic_publish($msg, '', 'hello');
    }
+
 }
